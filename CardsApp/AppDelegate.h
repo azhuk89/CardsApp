8a244d7a68600529047c20d1a64cc35fbc6994f5
Alexandr Zhuk <azhuk89@gmail.com>
2016-07-21 10:02:02 +0300

    added project wit pods

---
+++ CardsApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CardsApp
+//
+//  Created by Alexandr Zhuk on 7/20/16.
+//  Copyright © 2016 Alexandr Zhuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CardsApp/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  CardsApp
+//
+//  Created by Alexandr Zhuk on 7/20/16.
+//  Copyright © 2016 Alexandr Zhuk. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <AFNetworking.h>
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
